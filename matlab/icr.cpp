01730858036ff07b520dcd70080118b2f8b9a2e3
Robert Krug <robert.krug@oru.se>
2014-02-16 22:09:27 +0100

    first version of the mex interface is flying: needs compilation of qhulcpp with the -fPIC flag!

--- matlab/icr.cpp
+++ matlab/icr.cpp
@@ -1,9 +1,55 @@
 #include "../include/icr.h"
 #include "mex.h"
+
+using namespace ICR;
+
 void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
 {
 
+  //Load a new target object 
+   ObjectLoader obj_loader;
+   obj_loader.loadObject("../models/beer_can.obj","beer_can");
+ 
+   //Create a list of 5 default finger parameters (default parameters defined in config.h) and a 
+   //vector of centerpoint contact id's for the 5-fingered prototype grasp
+   FParamList f_parameters;
+   FingerParameters parameters;
+   for (int i=0;i<5;i++)
+     f_parameters.push_back(parameters);
+   VectorXui centerpoint_ids(5);
+   centerpoint_ids << 1838, 4526, 4362, 1083, 793;
 
-//using namespace ICR;
-//   ObjectLoader obj_loader;
+   //Create a prototype grasp and search zones, the parameter alpha is the scale of the largest
+   //origin-centered ball contained by the Grasp wrench space of the prototype grasp
+   GraspPtr prototype_grasp(new Grasp());
+   prototype_grasp->init(f_parameters,obj_loader.getObject(),centerpoint_ids);
+   SearchZonesPtr search_zones(new SearchZones(prototype_grasp));
+   double alpha=0.5;
+   search_zones->computeShiftedSearchZones(alpha);
+
+   //Create and plot the Independent Contact Regions
+   IndependentContactRegions icr(search_zones,prototype_grasp);
+   icr.computeICR();
+   std::cout<<icr;
+   //=====================================================================
+
+   //Load a new target object
+   obj_loader.loadObject("../models/cup.obj","cup");
+
+   //Modify the centerpoint id's and some of the finger parameters of the prototype-grasp.
+   //The third finger utilizies the Multi-Point contact model, patches only contain the center-point
+   //and border points
+   centerpoint_ids << 850, 305, 2009, 1740, 1701;     
+   f_parameters[0].setSoftFingerContact(1,6,0.5,0.5);
+   f_parameters[1].setFrictionlessContact(1);
+   f_parameters[2].setContactModelType(Multi_Point);
+   f_parameters[2].setInclusionRuleFilterPatch(true);
+
+   //Update prototype grasp, search zones and ICR
+   prototype_grasp->init(f_parameters,obj_loader.getObject(),centerpoint_ids);
+   search_zones->computeShiftedSearchZones(alpha);
+   icr.computeICR();
+   std::cout<<icr;
+
+
 }
--- matlab/make.m
+++ matlab/make.m
@@ -1,9 +1,12 @@
-IFLAGS  = [ '-I. -I../tools/eigen-eigen-3.0.2/ ' ];
+IFLAGS  = [ '-I. -I../tools/eigen-eigen-3.0.2/ -I../tools/Qhull-2011.2/src/libqhullcpp ' ];
 
-CPPFLAGS  = [ IFLAGS, '-D__cpluplus -O ' ];
+CPPFLAGS  = [IFLAGS, '-D__cpluplus -O ' ];
 
-ICR_OBJECTS =	[ 'icr.cpp '];
+ICR_OBJECTS =	[ 'icr.cpp ', '../lib/libicr.a ', '../tools/obj-1.0/obj/.libs/libobj.so ', ...
+                  '../tools/Qhull-2011.2/lib/libqhull6.so ', '../tools/Qhull-2011.2/lib/libqhullcpp.a '];
 
 eval( [ 'mex CXX=g++ -output icr ', CPPFLAGS, ICR_OBJECTS ] );
 
 disp( [ 'icr.', eval('mexext'), ' successfully created!'] );
+
+%mex CXX=g++ -output icr icr.cpp -I../tools/eigen-eigen-3.0.2/ -I../tools/Qhull-2011.2/src/libqhullcpp 
