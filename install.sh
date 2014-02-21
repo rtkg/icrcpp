61ca99a981106ecb8c84855f85d40a6b92bb5e11
Robert Krug <robert.krug@oru.se>
2014-02-21 18:22:05 +0100

    fixed shell script

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ cd tools/obj-1.0/
 ./configure --prefix=`pwd`/build
 make 
 make install
-#cd ../Qhull-2011.2/
-#cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build
-#make 
-#make install
-#make clean
-#cd ../../
-#make
-#cd examples
-#make
+cd ../Qhull-2011.2/
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build
+make 
+make install
+make clean
+cd ../../
+make
+cd examples
+make
