7493b84cddd1e5ea419d67b4b188ce563aafac82
rtkg <robert.krug@oru.se>
2014-02-21 18:37:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ git checkout master
 ./configure --prefix=`pwd`/build
 make 
 make install
-cd ../Qhull-2011.2/
-git checkout master
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build
-make 
-make install
-make clean
+#cd ../Qhull-2011.2/
+#git checkout master
+#cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build
+#make 
+#make install
+#make clean
 cd ../../
 make
 cd examples
