7dabe61ce33119fa1e1e9c7470e734b56e875ba6
Robert Krug <robert.krug@oru.se>
2014-02-21 18:27:52 +0100

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 git submodule init 
 git submodule update 
 cd tools/obj-1.0/
+git checkout master
 ./configure --prefix=`pwd`/build
 make 
 make install
 cd ../Qhull-2011.2/
+git checkout master
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/build
 make 
 make install
