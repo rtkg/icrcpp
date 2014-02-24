adad15a8ae89136ef02b956e90bff2b67fbc6920
Robert Krug <robert.krug@oru.se>
2014-02-24 11:46:36 +0100

    fixed installation of the ACADO toolkit

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 git submodule init 
 git submodule update 
 cd tools/obj-1.0/
-git checkout master
+#git checkout master
 ./configure --prefix=`pwd`/build
 make 
 make install
 cd ../Qhull-2011.2/
 make
 make clean
-cd ../../
+cd ..
+git clone https://github.com/acado/acado.git -b stable ACADOtoolkit
+cd ACADOtoolkit
+git checkout v1.2.1beta
+mkdir build
+cd build
+cmake ..
+make
+make clean
+cd ../../../
 make
 cd examples
 make
