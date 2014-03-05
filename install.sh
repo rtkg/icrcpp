2d305478a5d617270bb2536c0f555c19243a1ce4
rtkg <robert.krug@oru.se>
2014-03-06 00:28:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 git submodule init 
 git submodule update 
-git checkout feature-casadi
 cd tools/obj-1.0/
 ./configure --prefix=`pwd`/build
 make 
@@ -9,15 +8,7 @@ make install
 cd ../Qhull-2011.2/
 make
 make clean
-cd ..
-git clone https://github.com/acado/acado.git -b stable ACADOtoolkit
-cd ACADOtoolkit
-git checkout v1.2.1beta
-mkdir build
-cd build
-cmake ..
-make
-cd ../../../
+cd ../../
 make
 cd examples
 make
